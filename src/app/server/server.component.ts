e0761985955a4f6ff8011c2e107d800abaf95aea
Semyon Pleshivtsev <pleshivtsev@gmail.com>
2020-05-03 14:30:57 +0300

    Section 2 completed

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,5 +7,19 @@ import {Component} from '@angular/core';
 })
 
 export class ServerComponent {
+  status: any;
+
+  constructor() {
+    Math.random() > 0.5 ? this.status = 'on-line' : this.status = 'off-line';
+  }
+
+
+  getStatus(){
+    return Math.random() > 0.5 ? this.status ='on-line' : this.status ='off-line';
+  }
+
+  getColor(){
+    return this.status === 'on-line' ? 'green' : 'red';
+  }
 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // templateUrl: './servers.component.html',
-  template: `<app-server></app-server>,
-                <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  // template: `<app-server></app-server>,<app-server></app-server>`,
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
+  allowAddServer = false;
+  serverName = '';
+  userName = 'defaultUser';
+  serverCreated = false;
 
-  constructor() { }
+  constructor() {
+
+    setTimeout(() => {
+      this.allowAddServer = true;
+    }, 2000);
+
+  }
+
 
   ngOnInit(): void {
   }
